68374bca102c4fafce79269e37f6a57b6fd0d74f
EDAW412 <wade.wright412@gmail.com>
2025-06-29 18:38:08 +0800

    Update admin manage products.php

--- htdocs/admin manage products.php
+++ htdocs/admin manage products.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['staff_role']) || $_SESSION['staff_role'] !== 'admin') {
     exit();
 }
 
-$conn = new mysqli("sql110.infinityfree.com", "if0_39218282", "WaddlePaddle412", "if0_39218282_laezel_marketplace");
+$conn = new mysqli("sql110.infinityfree.com", "if0_39218282", "********", "if0_39218282_laezel_marketplace");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
